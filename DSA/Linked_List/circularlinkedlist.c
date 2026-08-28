fbdbd49f98ebdb8ccc3b5d6dcff531b37590fe74
Soumya ! <soumya.dip.adak@outlook.com>
2026-08-28 16:09:29 +0530

    linked list

--- DSA/Linked_List/circularlinkedlist.c
+++ DSA/Linked_List/circularlinkedlist.c
@@ -14,6 +14,11 @@ Node *tail = NULL;
 
 struct Node *createNode(int data) {
     Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(0);
+    }
+
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
@@ -55,7 +60,7 @@ void deleteFront() {
         return;
     }
 
-    if(head == tail) {
+    if (head == tail) {
         head = NULL;
         tail = NULL;
         return;
@@ -66,24 +71,107 @@ void deleteFront() {
 }
 
 void deleteRear() {
-    if(head == NULL) {
+    if (head == NULL) {
         printf("List is empty.\n");
         return;
     }
 
-    if(head == tail) {
+    if (head == tail) {
         head = NULL;
         tail = NULL;
         return;
     }
 
     Node *currentNode = head;
-    while(currentNode->next != tail) {
+    while (currentNode->next != tail) {
         currentNode = currentNode->next;
     }
     tail = currentNode;
     tail->next = head;
 }
 
+void deleteByValue(int val) {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
 
+    Node *currentNode = head;
 
+    do {
+        if (currentNode->data == val) {
+            Node *deleteNode = currentNode->next;
+
+            // only one node
+            if (deleteNode == currentNode) {
+                head = NULL;
+                tail = NULL;
+                free(deleteNode);
+                return;
+            }
+
+            // deleting tail
+            if (deleteNode == tail) {
+                tail = currentNode;
+            }
+
+            currentNode->next = deleteNode->next;
+
+            tail->next = head;
+
+            free(deleteNode);
+            return;
+        }
+
+        currentNode = currentNode->next;
+    } while (currentNode != head);
+
+    printf("Node %d not found.\n", val);
+}
+
+void deleteList() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    Node *currentNode = head;
+    Node *nextNode;
+    while (currentNode != NULL) {
+        nextNode = currentNode->next;
+        free(currentNode);
+        currentNode = nextNode;
+    }
+
+    free(tail);
+
+    head = NULL;
+    tail = NULL;
+
+    printf("Circular linked list deleted.\n");
+}
+
+void display() {
+    if (head == NULL) {
+        printf("List is empty.\n");
+        return;
+    }
+
+    Node *currentNode = head;
+    do {
+        printf("%d->", currentNode->data);
+        currentNode = currentNode->next;
+    } while (currentNode != head);
+    
+    printf("head\n");
+}
+
+int main() {
+    insertFront(20);
+    insertFront(30);
+    insertFront(40);
+    insertRear(50);
+    insertRear(60);
+    display();
+    return 0;
+}
